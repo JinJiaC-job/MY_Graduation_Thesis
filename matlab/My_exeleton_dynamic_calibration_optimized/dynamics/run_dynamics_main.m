26b9e32a0fe5ca62bd0c30fbdd1de92c9ae667d2
JinJiaC-job <1005075902@qq.com>
2024-02-18 14:54:55 +0800

    111

---
+++ matlab/My_exeleton_dynamic_calibration_optimized/dynamics/run_dynamics_main.m
@@ -0,0 +1,69 @@
+clear, clc, close all;
+
+%% 求解外骨骼机械臂动力学
+NewtonEuler_dynamic_tau;
+% Save to txt
+t1 = char(tau(1)); t2 = char(tau(2)); t3 = char(tau(3)); t4 = char(tau(4)); t5 = char(tau(5)); t6 = char(tau(6)); 
+fid = fopen('.\data\txt\dyn_inertia_link_jointAxis.txt', 'w');
+fprintf(fid, ...
+    'T1=%s\rT2=%s\rT3=%s\rT4=%s\rT5=%s\rT6=%s\r', ...
+    t1, t2, t3, t4, t5, t6);
+fclose(fid);
+% Save to mat
+clear fid t1 t2 t3 t4 t5 t6 ans;
+save('.\data\mat\dyn_inertia_link_jointAxis.mat');
+
+%% 动力学方程线性化：将dyn_param_linearization.txt的数据复制到 w_regressor.m中
+dyn_param_linearization; 
+% Save to txt
+fid = fopen('.\data\txt\dyn_param_linearization.txt','w');
+for i = 1:6
+    for j = 1:pnum_sum
+	    fprintf(fid, 'w%d%d=%s;\r', i, j, char(W(i, j)));
+    end
+end
+
+fclose(fid);
+% Save to mat
+clear fid i j k1 k2 Q_ ans;
+save('.\data\mat\dyn_param_linearization.mat')
+save('.\data\mat\regressor.mat', 'W');
+
+%% 推导最小回归矩阵W (QR 分解)
+% dyn_minimal_param_syms;  % (OR dyn_minimal_param_math)
+dyn_minimal_param_math;%生成dyn_minimal_param_math.txt文件，将其数据复制到excitation\min_regressor
+% Save to mat
+clear ans;
+save('.\data\mat\dyn_minimal_param_syms.mat');  % (OR save('.\data\mat\dyn_minimal_param_math.mat') )
+save('.\data\mat\min_regressor.mat', 'W_min');
+save('..\identify\data\min_regressor.mat', 'W_min');
+
+
+
+%% 最小参数集向标准参数集转换
+clear, clc, close all;
+load('.\data\mat\dyn_minimal_param_syms.mat'); % (OR load('.\data\mat\dyn_minimal_param_math.mat'))
+load('.\data\mat\least_square.mat');
+
+dyn_Pmin2P;
+
+save('.\data\mat\mapping_Pmin2P.mat');
+save('.\data\mat\P_link.mat', 'P_link');
+save('.\data\mat\P_center.mat', 'P_center');
+save('.\utils\gen_params\P_center.mat', 'P_center');
+save('.\utils\gen_params\P_link.mat', 'P_link');
+% Save to txt
+addpath('.\utils\')
+mat2txt('.\data\txt\P_center.txt', P_center);
+mat2txt('.\data\txt\P_link.txt', P_link);
+rmpath('.\utils\')
+
+
+%% 验证标准参数集
+load('.\data\mat\mapping_Pmin2P.mat');
+load('.\data\mat\excit_filtering.mat');
+
+compute_model_error;
+
+
+
---
+++ matlab/My_exeleton_dynamic_calibration_optimized/identify/least_square_estimation_math.m
@@ -0,0 +1,47 @@
+%% least_square_estimation.m
+% @brief: 用算术表达式的最小二乘法估计参数
+%% PARAMETER
+% load('.\data\filt.mat');
+
+% filtered data
+q_filt = evalin('base', 'q_filt');
+qd_filt = evalin('base', 'qd_filt');
+qdd_filt = evalin('base', 'qdd_filt');
+t_filt = evalin('base', 't_filt');
+% DH和负载参数 % 单位：mm
+fe1 = 0; fe2 = 0; fe3 = 0; ne1 = 0; ne2 = 0; ne3 = 0;
+d2 = -188.76; d3 = -81.24; d5 = 12.15; d6 = 112;
+a5 = 252.5; alp4 = 4/9*pi; g = 9802;
+
+% 最小参数集
+pnum_min = evalin('base', 'pnum_min');
+
+n = length(q_filt);		    % number of sampling points
+ww = zeros(n * 6, pnum_min);%每个采样时间点的Wmin集合
+TT = zeros(n * 6, 1);       %每个采样时间点的实验所测得实际关节扭矩
+for k = 1:n
+	q = q_filt(k, :);
+	qd = qd_filt(k, :);
+	qdd = qdd_filt(k, :);
+
+	row1 = 1+(k-1)*6;
+	row2 = 6+(k-1)*6;
+	ww(row1:row2, :) = min_regressor(q, qd, qdd);
+	TT(row1:row2, 1) = 1e3 * t_filt(k, :)';  % in Nmm and N（注意单位）
+end
+
+%% 映射到最小参数集
+% ww * P = TT  ->  P =  inv(ww.T*ww)*ww.T*TT
+% ww:(n*6, pnum_min), TT:(n*6, 1), P:(pnum_min, 1)
+P = ((ww' * ww)^(-1)) * ww' * TT;
+assignin('base', 'P_min', P);	% assign variable in workspace
+
+%% SAVE TO FILE
+% fid = fopen('.\data\P_min.txt', 'w');
+% fprintf(fid, 'P_min = [');
+% for j = 1:pnum_min
+%     fprintf(fid, '%s;', P(j));
+% end
+% fprintf(fid, '];');
+
+
